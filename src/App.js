9ab9ebc4774632a658102fe90bc25db2d293c7a2
ImGyuriKim <gyurikim0105@gmail.com>
2023-06-06 18:10:56 +0900

    fix: global styling component child error

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import "./App.css";
 import GlobalStyle from "./GlobalStyle";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
+import Todo from "./pages/Todo";
+import Nav from "./components/Nav";
+import React from "react";
 
 function App() {
   return (
-    <>
-      <GlobalStyle></GlobalStyle>
-    </>
+    <React.Fragment>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
+          <Route path="/signin" element={<Signin />}></Route>
+          <Route path="/todo" element={<Todo />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </React.Fragment>
   );
 }
 
