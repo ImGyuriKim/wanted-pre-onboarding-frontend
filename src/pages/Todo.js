2e26c3dc1cc80bd7c66a01dfb154854d47dc37b8
ImGyuriKim <gyurikim0105@gmail.com>
2023-06-07 23:09:39 +0900

    feat: http request for GET POST DELETE done

--- src/pages/Todo.js
+++ src/pages/Todo.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { useState, useEffect } from "react";
+import Input from "../components/Input";
 
 const Container = styled.div`
   display: flex;
@@ -7,18 +8,21 @@ const Container = styled.div`
   align-items: center;
   border: 1px solid grey;
   margin: 5vh;
-  .todoList {
-    list-style-type: none;
-    padding: 10px;
-    border: 1px solid grey;
-    margin-bottom: 1rem;
-  }
 `;
 
-const NewContainer = styled.div`
+const BtnContainer = styled.div`
   display: flex;
-  height: 5vh;
 `;
+
+const ListContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  list-style-type: none;
+  padding: 10px;
+  border: 1px solid grey;
+  margin-bottom: 1rem;
+`;
+
 function Todo() {
   const access_token = localStorage.getItem("access_token");
   const baseURL = "https://www.pre-onboarding-selection-task.shop/";
@@ -67,37 +71,90 @@ function Todo() {
       body: JSON.stringify({
         todo,
       }),
-    }).then((res) => console.log(res));
+    }).then((res) => {
+      console.log(res);
+      window.location.reload();
+    });
   };
 
+  // 체크박스 핸들러 함수
+  const [isEditing, setIsEditing] = useState(false);
+  const handleCheckBox = (e) => {
+    const id = e.target.value;
+    const todo = e.target.name;
+    fetch(`${baseURL}todos/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${access_token}`,
+      },
+      body: JSON.stringify({
+        todo: todo,
+        isCompleted: e.target.checked,
+      }),
+    })
+      .then((res) => res.json())
+      .then((res) => console.log(res))
+      .catch((error) => console.log(error));
+  };
+
+  // 삭제 핸들러 함수
+  const handleDelete = (e) => {
+    const id = e.target.value;
+    fetch(`${baseURL}todos/${id}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    }).then((res) => {
+      if (res.status === 204) {
+        alert("삭제되었습니다.");
+        window.location.reload();
+      }
+    });
+  };
   return (
     <Container>
       <h1>Todo List</h1>
-      <NewContainer>
-        <input
-          data-testid="new-todo-input"
-          onChange={(e) => {
-            setTodo(e.target.value);
-          }}
-        />
-        <button data-testid="new-todo-add-button" onClick={handleAddButton}>
-          추가
-        </button>
-      </NewContainer>
-
+      <Input setTodo={setTodo} handleAddButton={handleAddButton} />
       <ul className="todoLists">
-        <li className="todoList">
-          <label>
-            <input type="checkbox" />
-            <span>TODO 1</span>
-          </label>
-        </li>
-        <li className="todoList">
-          <label>
-            <input type="checkbox" />
-            <span>TODO 2</span>
-          </label>
-        </li>
+        {todos &&
+          todos.map((todo) => {
+            return isEditing ? (
+              <Input></Input>
+            ) : (
+              <ListContainer key={todo.id}>
+                <li className="todoList">
+                  <label>
+                    {todo.todo}
+                    <input
+                      type="checkbox"
+                      value={todo.id}
+                      name={todo.todo}
+                      onChange={(e) => handleCheckBox(e)}
+                    ></input>
+                  </label>
+                </li>
+                <BtnContainer>
+                  <button
+                    data-testid="modify-button"
+                    onClick={() => {
+                      setIsEditing(true);
+                    }}
+                  >
+                    수정
+                  </button>
+                  <button
+                    data-testid="delete-button"
+                    value={todo.id}
+                    onClick={(e) => handleDelete(e)}
+                  >
+                    삭제
+                  </button>
+                </BtnContainer>
+              </ListContainer>
+            );
+          })}
       </ul>
     </Container>
   );
